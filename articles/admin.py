52d90a483ec3b1ffba790edb524bec5740f186f3
kazemkhanlou <kazemkhanlou@gmail.com>
2025-02-01 22:53:01 +0330

    newspaper app

---
+++ articles/admin.py
@@ -0,0 +1,16 @@
+# articles/admin.py
+from django.contrib import admin
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "body",
+        "author",
+        "date",
+        "pk"
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
