755c570a7937e5f13d20d0969f8bd6870c8dacaa
kazemkhanlou <kazemkhanlou@gmail.com>
2025-02-02 02:19:13 +0330

    comments app

--- articles/admin.py
+++ articles/admin.py
@@ -1,16 +1,25 @@
 # articles/admin.py
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
     list_display = [
+        "id",
         "title",
         "body",
         "author",
         "date",
-        "pk"
     ]
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
